a9d06bc441a1599dbcb4568bfa32a72a560ae9a1
ying0801 <1029476703@qq.com>
2019-11-23 20:54:08 +0800

    通过eleUI新版本组件属性的配置，完成级联选择器，避免了用原生js

--- src/components/goods/goodsadd.vue
+++ src/components/goods/goodsadd.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2019-11-23 16:41:12
- * @LastEditTime: 2019-11-23 20:09:14
+ * @LastEditTime: 2019-11-23 20:52:05
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Vue.jsc:\编程\vuepro\mymall\src\components\goods\goodsadd.vue
@@ -56,11 +56,27 @@
         <el-form-item label="商品分类">
           <!-- 这里需要一个级联选择器，版本更新，这是2.12.0版的 -->
           <!-- <el-input v-model="form.goods_cat"></el-input> -->
-           <el-cascader
+           <!-- <el-cascader
            v-model="form.value"
            :options="form.options"
            :props="{ expandTrigger: 'hover' }"
-           ></el-cascader>
+          > -->
+           <el-cascader
+           v-model="form.value"
+           :options="form.options"
+           :props="{
+            expandTrigger: 'hover', label: 'cat_name',
+            value: 'cat_id',
+            children: 'children'}"
+          >
+          <!-- 旧版本的写法已经不能用了，remove了 -->
+           <!-- <el-cascader
+           props.expandTrigger="hover"
+           :options="form.selectedOptions"
+           v-model="form.value"
+           :props="form.defaultProp"
+           > -->
+           </el-cascader>
         </el-form-item>
       </el-tab-pane>
       <el-tab-pane label="商品参数" name="2">商品参数</el-tab-pane>
@@ -88,20 +104,15 @@ export default {
         attrs: '',
         // 级联选择器使用的数据
         value: [],
-        options: [{
-          value: 'ziyuan',
-          label: '资源',
-          children: [{
-            value: 'axure',
-            label: 'Axure Components'
-          }, {
-            value: 'sketch',
-            label: 'Sketch Templates'
-          }, {
-            value: 'jiaohu',
-            label: '组件交互文档'
-          }]
-        }]
+        options: []
+        // 旧版本有的属性
+        // options: [],
+        // selectedOptions: [],
+        // defaultProp: {
+        //   label: 'cat_name',
+        //   value: 'cat_id',
+        //   children: 'children'
+        // }
       }
     }
   },
@@ -113,37 +124,43 @@ export default {
     // }
     // 获取所有商品分类 categories type [1,2,3] 值：1，2，3 分别表示显示一层二层三层分类列表
     async getCategories () {
-      const res = await this.$http.get(`categories`)
-      const catAll = res.data.data
-      var myoptions = []
-      catAll.forEach(item1 => {
-        var tmpArr = []
-        var tmpObj = {}
-        tmpObj.value = item1.cat_id
-        tmpObj.label = item1.cat_name
-        if (item1.children) {
-          item1.children.forEach(item2 => {
-            var tmpObj2 = {}
-            var tmpArr2 = []
-            tmpObj2.value = item2.cat_id
-            tmpObj2.label = item2.cat_name
-            if (item2.children) {
-              item2.children.forEach(item3 => {
-                var tmpObj3 = {}
-                tmpObj3.value = item3.cat_id
-                tmpObj3.label = item3.cat_name
-                tmpArr2.push(tmpObj3)
-              })
-            }
-            tmpObj2.children = tmpArr2
-            tmpArr.push(tmpObj2)
-          })
-        }
-        tmpObj.children = tmpArr
-        // console.log('每一回遍历得到的', tmpObj)
-        myoptions.push(tmpObj)
-      })
-      this.form.options = myoptions
+      const res = await this.$http.get(`categories?type=3`)
+      this.form.options = res.data.data
+      // console.log('form.options', this.form.options)
+      // 原生js嵌套遍历
+      // var myoptions = []
+      // const catAll = res.data.data
+      // catAll.forEach(item1 => {
+      //   var tmpArr = []
+      //   var tmpObj = {}
+      //   tmpObj.value = item1.cat_id
+      //   tmpObj.label = item1.cat_name
+      //   if (item1.children) {
+      //     item1.children.forEach(item2 => {
+      //       var tmpObj2 = {}
+      //       var tmpArr2 = []
+      //       tmpObj2.value = item2.cat_id
+      //       tmpObj2.label = item2.cat_name
+      //       if (item2.children) {
+      //         item2.children.forEach(item3 => {
+      //           var tmpObj3 = {}
+      //           tmpObj3.value = item3.cat_id
+      //           tmpObj3.label = item3.cat_name
+      //           tmpArr2.push(tmpObj3)
+      //           // 思路：一直顺着写到最里面的叶子节点，形成了对象，放到数组里
+      //         })
+      //       }
+      //       tmpObj2.children = tmpArr2
+      //       // 赋值给父节点的children
+      //       tmpArr.push(tmpObj2)
+      //       // label，value2个属性早就准备好，现在children也好了，爷爷形成对象，放到数组里
+      //     })
+      //   }
+      //   tmpObj.children = tmpArr
+      //   // console.log('每一回遍历得到的', tmpObj)
+      //   myoptions.push(tmpObj)
+      // })
+      // this.form.options = myoptions
     //   console.log('商品分类', res)
     //   console.log('options', this.form.options)
     }
